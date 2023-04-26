2a6009b61780283a091f2a2c685e706a71c96307
C-FWES <353861933@educ.dpcdsb.org>
2023-04-26 14:53:48 -0400

    chore: clean

--- src/toc.js
+++ src/toc.js
@@ -7,9 +7,6 @@ import tags from './icons/tag.png';
 
 function TableOfContents(props) {
 
-  // const colors = randomColor({ count: 5, luminosity: 'light', hue: 'random' });
-  // const { hue, saturation, lightness } = randomColor({ hue: colors[0], format: 'hsl' });
-
   return (
     <div className="tableOfContents">
       <div className="tableInfo">
@@ -19,14 +16,6 @@ function TableOfContents(props) {
         <p className="tocTitle" id="breakSection">
         {/* <img src={tags} className="tocIcon"></img> */}
           <span className="tocInnerText">Topics</span></p>
-        {/* <div className="fileSys">
-          {props.fileNames.map((file, index) => (
-            <button
-              key={index}
-              className="fileElem"
-              onClick={() => props.handleClick(file)}>{file.replace(/^.*[\\/]/, '')}</button>
-          ))}
-        </div> */}
  <div className="fileViewTopicsContainer">
       <button className="topic" >
         chemistry
@@ -55,25 +44,6 @@ function TableOfContents(props) {
         </div>
         <br></br>
         <br></br>
-        {/* <p className='tocTitle'>Outline</p>
-        <div>
-          {
-            props.tocHeaders.map((header, index) => (
-              <div key={index} className="outlineElement">
-                <a href={`#${header.id}`} className="headerNav">
-                  <span className="headerDelim">
-                    {
-                      header.type === 'H2' ? '## ' :
-                        header.type === 'H3' ? '### ' :
-                          header.type === 'H4' ? '#### ' :
-                            header.type === 'H5' ? '##### ' :
-                              header.type === 'H6' ? '###### ' : '# '}
-                  </span>
-                  {header.text}
-                </a>
-              </div>
-            ))}
-        </div> */}
       </div>
     </div>
   );
