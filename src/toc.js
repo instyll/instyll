17d7cef0f2d6d61e2bb6c9aca80ad690efc3df57
C-FWES <353861933@educ.dpcdsb.org>
2023-04-27 16:28:46 -0400

    TOC: add home, recent, templates, document icons

--- src/toc.js
+++ src/toc.js
@@ -4,42 +4,58 @@ import randomColor from 'randomcolor';
 
 // assets
 import tags from './icons/tag.png';
+import home from './icons/home.png';
+import recent from './icons/recent.png';
+import template from './icons/template.png';
+import favorites from './icons/starMenu.png';
+import note from './icons/note.png';
 
 function TableOfContents(props) {
 
   return (
     <div className="tableOfContents">
       <div className="tableInfo">
-        <p className="tocTitleFirst"><span className="tocInnerText">Home</span></p>
-        <p className="tocTitle"><span className="tocInnerText">Recent</span></p>
-        <p className="tocTitle"><span className="tocInnerText">Templates</span></p>
+        <p className="tocTitleFirst">
+          <img src={home} className="tocIcon"></img>
+          <span className="tocInnerText">Home</span></p>
+        <p className="tocTitle">
+          <img src={recent} className="tocIcon"></img>
+          <span className="tocInnerText">Recent</span></p>
+        <p className="tocTitle">
+          <img src={template} className="tocIcon"></img>
+          <span className="tocInnerText">Templates</span></p>
         <p className="tocTitle" id="breakSection">
-        {/* <img src={tags} className="tocIcon"></img> */}
+          <img src={tags} className="tocIcon"></img>
           <span className="tocInnerText">Topics</span></p>
- <div className="fileViewTopicsContainer">
-      <button className="topic" >
-        chemistry
-      </button>
-      <button className="topic" >
-        physics
-      </button>
-      <button className="topic" >
-        projects
-      </button>
-      <button className="topic" >
-        english
-      </button>
-      <button className="topic" >
-        marketing
-      </button>
-    </div>
-        <p className="tocTitle" id="breakSection">Starred</p>
+        <div className="fileViewTopicsContainer">
+          <button className="topic" >
+            chemistry
+          </button>
+          <button className="topic" >
+            physics
+          </button>
+          <button className="topic" >
+            projects
+          </button>
+          <button className="topic" >
+            english
+          </button>
+          <button className="topic" >
+            marketing
+          </button>
+        </div>
+        <p className="tocTitle" id="breakSection">
+          <img src={favorites} className="tocIcon"></img>
+          <span className="tocInnerText">Starred</span></p>
         <div className="fileSys">
           {props.fileNames.map((file, index) => (
             <button
               key={index}
               className="fileElem"
-              onClick={() => props.handleClick(file)}>{file.replace(/^.*[\\/]/, '')}</button>
+              onClick={() => props.handleClick(file)}>
+              <img src={note} className="tocIcon"></img>
+              <span className="tocInnerText">{file.replace(/^.*[\\/]/, '')}</span>
+            </button>
           ))}
         </div>
         <br></br>
