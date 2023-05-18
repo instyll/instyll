04a0b1edce25cc42d8d80ef6aada1196eabec68d
C-FWES <353861933@educ.dpcdsb.org>
2023-05-18 17:30:10 -0400

    fix modal z index

--- src/TopicModal.js
+++ src/TopicModal.js
@@ -2,33 +2,38 @@ import React, { useState } from 'react';
 import Modal from 'react-modal';
 
 const TopicModal = ({ show, onHide }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
-  const [selectedTags, setSelectedTags] = useState([]);
-  const [newTag, setNewTag] = useState('');
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
+    const [selectedTags, setSelectedTags] = useState([]);
+    const [newTag, setNewTag] = useState('');
 
-  const handleTagSelect = (tag) => {
-    if (selectedTags.includes(tag)) {
-      setSelectedTags(selectedTags.filter((selectedTag) => selectedTag !== tag));
-    } else {
-      setSelectedTags([...selectedTags, tag]);
-    }
-  };
+    const handleTagSelect = (tag) => {
+        if (selectedTags.includes(tag)) {
+            setSelectedTags(selectedTags.filter((selectedTag) => selectedTag !== tag));
+        } else {
+            setSelectedTags([...selectedTags, tag]);
+        }
+    };
 
-  const handleNewTagChange = (event) => {
-    setNewTag(event.target.value);
-  };
+    const handleNewTagChange = (event) => {
+        setNewTag(event.target.value);
+    };
 
-  const handleAddTag = () => {
-    if (newTag && !tags.includes(newTag)) {
-      setTags([...tags, newTag]);
-      setSelectedTags([...selectedTags, newTag]);
-      setNewTag('');
-    }
-  };
+    const handleAddTag = () => {
+        if (newTag && !tags.includes(newTag)) {
+            setTags([...tags, newTag]);
+            setSelectedTags([...selectedTags, newTag]);
+            setNewTag('');
+        }
+    };
 
-  return (
-      <Modal isOpen={show} onRequestClose={onHide}>
+    return (
+        <Modal isOpen={show} onRequestClose={onHide} style={{
+            overlay: {
+                backgroundColor: "rgba(0,0,0,0.4)",
+                zIndex: "999",
+            },
+            }}>
         <h2>Select Tags</h2>
         <ul className="tags">
           {tags.map((tag) => (
@@ -51,7 +56,7 @@ const TopicModal = ({ show, onHide }) => {
           <button onClick={handleAddTag}>Add Tag</button>
         </div>
         <button onClick={() => setModalIsOpen(false)}>Close</button>
-      </Modal>
+      </Modal >
   );
 };
 
