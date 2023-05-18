8e85f9a82d1c7f39609acbf9b2c04fa9b8eee25c
C-FWES <353861933@educ.dpcdsb.org>
2023-05-18 18:40:13 -0400

    Fix topic modal position

--- src/TopicModal.js
+++ src/TopicModal.js
@@ -34,34 +34,54 @@ const TopicModal = ({ show, onHide }) => {
     return (
         <Modal isOpen={show} onRequestClose={onHide} style={{
             overlay: {
-                backgroundColor: "rgba(0,0,0,0.4)",
+                backgroundColor: "transparent",
                 zIndex: "999",
+                // backdropFilter: "blur(8px)",
             },
-            }}>
-        <h2>Select Tags</h2>
-        <ul className="tags">
-          {tags.map((tag) => (
-            <li
-              key={tag}
-              className={selectedTags.includes(tag) ? 'selected' : ''}
-              onClick={() => handleTagSelect(tag)}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-        <div className="new-tag">
-          <input
-            type="text"
-            placeholder="New Tag"
-            value={newTag}
-            onChange={handleNewTagChange}
-          />
-          <button onClick={handleAddTag}>Add Tag</button>
-        </div>
-        <button onClick={handleClose}>Close</button>
-      </Modal >
-  );
+            content: {
+                backgroundColor: "var(--elevated-bg)",
+                color: "var(--primary-text)",
+                fontFamily: "Inter",
+                borderRadius: "10px",
+                boxShadow: "rgba(0, 0, 0, 0.2) 0px 2px 4px 0px",
+                border: "0px none",
+                fontSize: "1em",
+                boxSizing: "border-box",
+                width: "400px",
+                // height: "calc(100% - 190px)",
+                height: "400px",
+                position: "absolute",
+                marginLeft: "290px",
+                marginTop: "100px",
+                // top: "50%",
+                // left: "50%",
+                // transform: "translate(-50%, -50%)",
+            }
+        }}>
+            <h2>Select Tags</h2>
+            <ul className="tags">
+                {tags.map((tag) => (
+                    <li
+                        key={tag}
+                        className={selectedTags.includes(tag) ? 'selected' : ''}
+                        onClick={() => handleTagSelect(tag)}
+                    >
+                        {tag}
+                    </li>
+                ))}
+            </ul>
+            <div className="new-tag">
+                <input
+                    type="text"
+                    placeholder="New Tag"
+                    value={newTag}
+                    onChange={handleNewTagChange}
+                />
+                <button onClick={handleAddTag}>Add Tag</button>
+            </div>
+            <button onClick={handleClose}>Close</button>
+        </Modal >
+    );
 };
 
 export default TopicModal;
