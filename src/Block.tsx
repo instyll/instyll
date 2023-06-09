3b9351fe10d96b4c450b57d815ef688826be608a
Wesley <choppakangaroo@gmail.com>
2023-06-09 12:53:52 -0400

    Clean

--- src/Block.tsx
+++ src/Block.tsx
@@ -54,7 +54,7 @@ export const Block = () => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            strokeWidth={1.5}
+            // strokeWidth={1.5}
             stroke="currentColor"
             className="blockSVG"
           >
