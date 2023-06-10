a74f752a286be4cde75474d1692f273ce78c5e2c
C-FWES <353861933@educ.dpcdsb.org>
2023-06-09 21:31:19 -0400

    Clean

--- src/Block.tsx
+++ src/Block.tsx
@@ -22,7 +22,7 @@ export const Block = () => {
         ctx: get().ctx,
         content: element,
         tippyOptions: {
-          zIndex: 999,
+          zIndex: 1,
           appendTo: document.body,
           onBeforeUpdate: () => setShowMenu(false),
           onClickOutside: () => setShowMenu(false),
@@ -49,6 +49,7 @@ export const Block = () => {
         )}
         ref={setElement}
       >
+
         <div onClick={() => setShowMenu((x) => !x)} className="blockSVGContainer">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -61,54 +62,6 @@ export const Block = () => {
             <path fill="none" d="M0 0h24v24H0V0z"></path><path d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
           </svg>
         </div>
-        {showMenu && (
-          <div className="absolute top-full mt-2 w-60 cursor-pointer rounded border-2 bg-gray-50 shadow dark:border-gray-900 dark:bg-gray-900">
-            <div
-              onClick={() => {
-                if (loading) return;
-
-                const commands = get().ctx.get(commandsCtx);
-                commands.call(wrapInHeadingCommand.key, 1);
-              }}
-              className="px-6 py-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-            >
-              Heading 1
-            </div>
-            <div
-              onClick={() => {
-                if (loading) return;
-
-                const commands = get().ctx.get(commandsCtx);
-                commands.call(wrapInHeadingCommand.key, 2);
-              }}
-              className="px-6 py-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-            >
-              Heading 2
-            </div>
-            <div
-              onClick={() => {
-                if (loading) return;
-
-                const commands = get().ctx.get(commandsCtx);
-                commands.call(wrapInHeadingCommand.key, 3);
-              }}
-              className="px-6 py-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-            >
-              Heading 3
-            </div>
-            <div
-              onClick={() => {
-                if (loading) return;
-
-                const commands = get().ctx.get(commandsCtx);
-                commands.call(turnIntoTextCommand.key);
-              }}
-              className="px-6 py-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-            >
-              Text
-            </div>
-          </div>
-        )}
       </div>
     </div>
   );
