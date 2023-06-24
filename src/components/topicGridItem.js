651d62527ec2d09c479e0ac7f2f4c5f144ccb65d
C-FWES <353861933@educ.dpcdsb.org>
2023-06-24 16:45:55 -0400

    UI improvements on topic grid item

--- src/components/topicGridItem.js
+++ src/components/topicGridItem.js
@@ -8,14 +8,15 @@ import '../App.css';
 const TopicGridItem = ({ tag }) => {
   return (
     <li className='topicItem' key={tag}>
-      <div className="topicTitleContainer">
-        <div className='optionObjectLeft'>
+      <div className='topicTextContainer'>
+        <div className='topicTextWrapper'>
           <div className='topicTitle'>
             <span>{tag}</span>
           </div>
-        </div>
-        <div className='optionObjectRight'>
-        </div>
+          <div className='topicInfo'>
+            <span>3 Notes</span>
+          </div>
+      </div>
       </div>
     </li>
   );
