133a9ac0cd6a75a3f02f1c9358c124f6cc474fd6
C-FWES <353861933@educ.dpcdsb.org>
2023-06-24 16:51:55 -0400

    fix missing fonts

--- src/components/topics.js
+++ src/components/topics.js
@@ -302,6 +302,7 @@ const Topics = () => {
                                                 ...baseStyles,
                                                 borderRadius: "10px",
                                                 backgroundColor: "var(--elevated-bg)",
+                                                fontFamily: "var(--font)",
                                             }),
                                             singleValue: (baseStyles, state) => ({
                                                 ...baseStyles,
