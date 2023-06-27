87ab6c81621312b5c4a29694b5df18bcfb0e417e
C-FWES <353861933@educ.dpcdsb.org>
2023-06-27 15:30:06 -0400

    More TeX commands

--- src/legacy/TeXOptions.js
+++ src/legacy/TeXOptions.js
@@ -45,35 +45,96 @@ const options = [
     { label: "\\bigtriangleup", type: "text", apply: "\\bigtriangleup" },    
     { label: "\\biguplus", type: "text", apply: "\\biguplus" },   
     { label: "\\bigvee", type: "text", apply: "\\bigvee" },   
-    
-    
+    { label: "\\begin{aligned}", type: "text", apply: "\\begin{aligned}" },
+    { label: "\\breve{}", type: "text", apply: "\\breve{}" },
+
+
+    // C
+    { label: "\\check{}", type: "text", apply: "\\check{}" },
+
+    // D
+    { label: "\\dot{}", type: "text", apply: "\\dot{}" },
+    { label: "\\ddot{}", type: "text", apply: "\\ddot{}" },
+    { label: "\\downarrow", type: "text", apply: "\\downarrow" },
+
+    // E
+    { label: "\\end{aligned}", type: "text", apply: "\\end{aligned}" },
+
+    // F
     { label: "\\frac{}{}", type: "text", apply: "\\frac{a}{b}" },    
-
-
-
-
     { label: "\\forall", type: "text", apply: "\\forall" },
+
+    // G
+    { label: "\\grave{}", type: "text", apply: "\\grave{}" },
+
+    // H
+    { label: "\\hat", type: "text", apply: "\\hat{}" },
+
+    // I
     { label: "\\infty", type: "text", apply: "\\infty" },
     { label: "\\int", type: "text", apply: "\\int" },
     { label: "\\iint", type: "text", apply: "\\iint" },
     { label: "\\iiint", type: "text", apply: "\\iiint" },
 
-    { label: "\\hat", type: "text", apply: "\\hat{}" },
+    // J
 
+    // K
+
+    // L
     { label: "\\limits", type: "text", apply: "\\limits" },
     { label: "\\log", type: "text", apply: "\\log" },
 
+    // M
+    { label: "\\mathring{}", type: "text", apply: "\\mathring{}" },
 
+    // N
+
+    // O
     { label: "\\oint", type: "text", apply: "\\oint" },
     { label: "\\overline", type: "text", apply: "\\overline" },
-    { label: "\\overrightarrow", type: "text", apply: "\\overrightarrow" },
+    { label: "\\overrightarrow{}", type: "text", apply: "\\overrightarrow{}" },
+    { label: "\\overrightharpoon{}", type: "text", apply: "\\overrightharpoon{}" },
+    { label: "\\Overrightarrow{}", type: "text", apply: "\\Overrightarrow{}" },
+    { label: "\\overleftarrow{}", type: "text", apply: "\\overleftarrow{}" },
+    { label: "\\overleftrightarrow{}", type: "text", apply: "\\overleftrightarrow{}" },
+    { label: "\\overbrace{}", type: "text", apply: "\\overbrace{}" },
 
+    // P
+    { label: "\\prime", type: "text", apply: "\\prime" },
+
+
+    // Q
+
+    // R
+
+    // S
     { label: "\\sum", type: "text", apply: "\\sum" },
 
+    // T
+    { label: "\\tilde{}", type: "text", apply: "\\tilde{}" },
 
+    // U
+    { label: "\\utilde{}", type: "text", apply: "\\utilde{}" },
+    { label: "\\underleftarrow{}", type: "text", apply: "\\underleftarrow{}" },
+    { label: "\\underleftrightarrow{}", type: "text", apply: "\\underleftrightarrow{}" },
+    { label: "\\underline{}", type: "text", apply: "\\underline{}" },
+    { label: "\\underbrace{}", type: "text", apply: "\\underbrace{}" },
+    { label: "\\underbar{}", type: "text", apply: "\\underbar{}" },
+    { label: "\\uparrow", type: "text", apply: "\\uparrow" },
+    { label: "\\updownarrow", type: "text", apply: "\\updownarrow" },
 
-    { label: "\\begin{aligned}", type: "text", apply: "\\begin{aligned}" },
-    { label: "\\end{aligned}", type: "text", apply: "\\end{aligned}" },
+    // V
+    { label: "\\vec{}", type: "text", apply: "\\vec{}" },
+
+    // W
+    { label: "\\widehat{}", type: "text", apply: "\\widehat{}" },
+    { label: "\\widecheck{}", type: "text", apply: "\\widecheck{}" },
+
+    // X
+
+    // Y
+
+    // Z
 ];
 
 module.exports = options;
