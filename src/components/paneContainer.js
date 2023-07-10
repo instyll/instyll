3711349c49117b143d9b921f0b711ffb0f1c4c69
C-FWES <353861933@educ.dpcdsb.org>
2023-07-10 14:29:49 -0400

    Fix pdf uploading

--- src/components/paneContainer.js
+++ src/components/paneContainer.js
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import '../App.css';
 import pdf from '../icons/pdf.png';
 
-const PdfUploadButton = ({ onFileSelect }) => {
+const PdfUploadButton = () => {
     const fileInputRef = useRef(null);
+    const [filePath, setFilePath] = useState(null);
 
     const handleButtonClick = () => {
         fileInputRef.current.click();
@@ -12,25 +13,36 @@ const PdfUploadButton = ({ onFileSelect }) => {
     const handleFileSelect = (event) => {
         const file = event.target.files[0];
         if (file && file.type === 'application/pdf') {
-            onFileSelect(file);
+            const fileURL = URL.createObjectURL(file);
+            setFilePath(fileURL);
+            console.log(fileURL);
         }
     };
 
     return (
-        <div className='selectPDFButtonContainer'>
-            <button
-                className='selectPDFButton'
-                onClick={handleButtonClick}>
-                    <img src={pdf} className='buttonIcon'></img>
+        <div>
+            <div className='selectPDFButtonContainer'>
+                <button
+                    className='selectPDFButton'
+                    onClick={handleButtonClick}
+                >
+                    <img src={pdf} className='buttonIcon' alt="PDF Icon" />
                     <span className='buttonText'>Select PDF</span>
-                    </button>
-            <input
-                type="file"
-                ref={fileInputRef}
-                accept="application/pdf"
-                style={{ display: 'none' }}
-                onChange={handleFileSelect}
-            />
+                </button>
+                <input
+                    type="file"
+                    ref={fileInputRef}
+                    accept="application/pdf"
+                    style={{ display: 'none' }}
+                    onChange={handleFileSelect}
+                />
+            </div>
+            <div className='pdfDisplayContainer'>
+                {/* Render the file path or the PDF */}
+                {filePath && (
+                    <iframe src={filePath} title="PDF Preview" width="auto" height="auto" />
+                )}
+            </div>
         </div>
     );
 };
