680b9281f6d104c49db05373692184c7b1079cc6
C-FWES <353861933@educ.dpcdsb.org>
2023-07-10 11:55:57 -0400

    Style button

--- src/components/paneContainer.js
+++ src/components/paneContainer.js
@@ -1,32 +1,38 @@
 import React, { useRef } from 'react';
+import '../App.css';
+import pdf from '../icons/pdf.png';
 
 const PdfUploadButton = ({ onFileSelect }) => {
-  const fileInputRef = useRef(null);
+    const fileInputRef = useRef(null);
 
-  const handleButtonClick = () => {
-    fileInputRef.current.click();
-  };
+    const handleButtonClick = () => {
+        fileInputRef.current.click();
+    };
 
-  const handleFileSelect = (event) => {
-    const file = event.target.files[0];
-    if (file && file.type === 'application/pdf') {
-      onFileSelect(file);
-    }
-  };
+    const handleFileSelect = (event) => {
+        const file = event.target.files[0];
+        if (file && file.type === 'application/pdf') {
+            onFileSelect(file);
+        }
+    };
 
-  return (
-    <div>
-      <button onClick={handleButtonClick}>Select PDF</button>
-      <input
-      className='selectPDFButton'
-        type="file"
-        ref={fileInputRef}
-        accept="application/pdf"
-        style={{ display: 'none' }}
-        onChange={handleFileSelect}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <button
+                className='selectPDFButton'
+                onClick={handleButtonClick}>
+                    <img src={pdf} className='buttonIcon'></img>
+                    Select PDF
+                    </button>
+            <input
+                type="file"
+                ref={fileInputRef}
+                accept="application/pdf"
+                style={{ display: 'none' }}
+                onChange={handleFileSelect}
+            />
+        </div>
+    );
 };
 
 export default PdfUploadButton;
