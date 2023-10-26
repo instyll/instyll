58a113a0a05f9937ce63c85d16a3beedf860d703
C-FWES <353861933@educ.dpcdsb.org>
2023-10-26 12:46:24 -0400

    Clean up

--- src/zap-menu/state.ts
+++ src/zap-menu/state.ts
@@ -11,7 +11,7 @@ export const useSlashState = (instance: Instance) => {
   const selectedRef = useRef(0);
   const [search, setSearch] = useState("");
 
-  const zapList = ["Hello", "Mathematics"];
+  const zapList = ["Hello", "Mathematics", "Good morning", "Thanks", "Thank you", "Nice", "Javascript", "Music", "Cookies"];
 
   const onPick = useCallback(
     (ctx: Ctx) => {
