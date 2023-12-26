c39f572bd5c3f568dd7189210b687256a9e66c61
C-FWES <353861933@educ.dpcdsb.org>
2023-12-26 17:19:44 -0500

    add modals to list component

--- src/components/document/documentListItem.js
+++ src/components/document/documentListItem.js
@@ -3,15 +3,37 @@
  */
 import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import DocumentOptionsModal from '../../modal/DocumentOptionsModal';
 import moreDots from '../../icons/menudots.png';
 import '../../App.css';
+import { useState } from 'react';
 
 const DocumentListItem = ({ documentInfo }) => {
 
   const navigate = useNavigate();
 
+  const [documentOptionsModalOpen, setDocumentOptionsModalOpen] = useState(false);
+  const [selectedDocument, setSelectedDocument] = useState(null);
+
+  const handleDocumentOptionsModalOpen = (value) => {
+    setDocumentOptionsModalOpen(true);
+  }
+
+  const handleClick = (e) => {
+    e.stopPropagation();
+    handleDocumentOptionsModalOpen(true);
+    setSelectedDocument(documentInfo);
+  }
+
   return (
-    <div className='topicListItem' key={documentInfo[0]} onClick={() => navigate(`/topics/${tag}`)}>
+    <div className='topicListItem' key={documentInfo[0]}>
+      
+      <DocumentOptionsModal
+        show={documentOptionsModalOpen}
+        selectedDocument={selectedDocument}
+        onHide={() => setDocumentOptionsModalOpen(false)}
+      />
+      
       <div className='topicListTextContainer'>
         <div className='topicListTextWrapper'>
           <div className='topicListTitle'>
@@ -20,7 +42,7 @@ const DocumentListItem = ({ documentInfo }) => {
           <div className='topicListInfo'>
             <span>{documentInfo[2]}</span> 
           </div>
-          <div className='topicOptionsMenuContainer' >
+          <div className='topicOptionsMenuContainer' onClick={handleClick}>
             <img src={moreDots} className='moreDots'></img>
           </div>
       </div>
