354d81da6b20dff551b2abb0ddf9a450d9907c41
C-FWES <353861933@educ.dpcdsb.org>
2024-03-20 20:02:07 -0400

    Update for topic list component

--- src/components/topic/topicListItem.js
+++ src/components/topic/topicListItem.js
@@ -3,19 +3,23 @@
  */
 import React, { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import TopicOptionsModal from '../../modal/topic/TopicOptionsModal';
 import moreDots from '../../icons/menudots.png';
 import '../../App.css';
 import { Menu, Item, Separator, Submenu, useContextMenu } from 'react-contexify';
+import { removeTag } from '../../tagSlice';
+import UpdateTopicModal from '../../modal/topic/UpdateTopicModal';
 import 'react-contexify/ReactContexify.css';
 
 const TopicListItem = ({ tag }) => {
 
   const [topicOptionsModalOpen, setTopicOptionsModalOpen] = useState(false);
   const [selectedTopic, setSelectedTopic] = useState(null);
+  const [updateTopicModalOpen, setUpdateTopicModalOpen] = useState(false);
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   // context menu
 
@@ -58,6 +62,11 @@ const TopicListItem = ({ tag }) => {
     setTopicOptionsModalOpen(true);
   }
 
+  //delete topic
+  const handleRemoveTag = (tagItem) => {
+    dispatch(removeTag(tagItem));
+  }
+
   const handleClick = (e) => {
     e.stopPropagation();
     show({
@@ -69,12 +78,16 @@ const TopicListItem = ({ tag }) => {
     setSelectedTopic(tag);
   }
 
+  const handleClose = () => {
+    setUpdateTopicModalOpen(false);
+  };
+
   return (
     <div className='topicListItem' key={tag} >
 
     <Menu id={tag}>
-        <Item id="rename" onClick={handleItemClick}>Rename</Item>
-        <Item id="delete" onClick={handleItemClick}>Delete</Item>
+        <Item id="rename" onClick={() => setUpdateTopicModalOpen(true)}>Rename</Item>
+        <Item id="delete" onClick={() => handleRemoveTag(selectedTopic)}>Delete</Item>
     </Menu>
 
       {/* <TopicOptionsModal
@@ -82,6 +95,13 @@ const TopicListItem = ({ tag }) => {
         selectedTopic={selectedTopic}
         onHide={() => setTopicOptionsModalOpen(false)}
       /> */}
+
+      <UpdateTopicModal
+      show={updateTopicModalOpen}
+      selectedTag={selectedTopic}
+      onHide={() => setTopicOptionsModalOpen(false)}
+      handleClose={handleClose}
+       />
 
       <div className='topicListTextContainer' onClick={() => navigate(`/topics/${tag}`)}>
         <div className='topicListTextWrapper'>
