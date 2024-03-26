afb64fd0547453f1ed6664d67542e11b25404651
C-FWES <353861933@educ.dpcdsb.org>
2024-03-26 13:45:16 -0400

    fix: list node styles

--- src/components/editor/ListItem.tsx
+++ src/components/editor/ListItem.tsx
@@ -1,3 +1,35 @@
+// import { useNodeViewContext } from "@prosemirror-adapter/react";
+// import type { FC } from "react";
+
+// export const ListItem: FC = () => {
+//   const { contentRef, node, setAttrs, selected } = useNodeViewContext();
+//   const { attrs } = node;
+//   const checked = attrs?.checked;
+//   const isBullet = attrs?.listType === "bullet";
+//   return (
+//     <li
+//       className={[ "checkboxNode",
+//         selected ? "ProseMirror-selectednode" : "",
+//       ].join(" ")}
+//     >
+//       <span className="checkboxSpan">
+//         {checked != null ? (
+//           <input
+//             className="checkboxInput"
+//             onChange={() => setAttrs({ checked: !checked })}
+//             type="checkbox"
+//             checked={checked}
+//           />
+//         ) : isBullet ? (
+//           <span className="" />
+//         ) : (
+//           <span className="">{attrs?.label}</span>
+//         )}
+//       </span>
+//       <div className="checkboxContent" ref={contentRef} />
+//     </li>
+//   );
+// };
 import { useNodeViewContext } from "@prosemirror-adapter/react";
 import type { FC } from "react";
 
@@ -8,25 +40,29 @@ export const ListItem: FC = () => {
   const isBullet = attrs?.listType === "bullet";
   return (
     <li
-      className={[ "checkboxNode",
-        selected ? "ProseMirror-selectednode" : "",
-      ].join(" ")}
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "start",
+        gap: "0.6rem",
+        ...(selected && { outline: "1px solid #000" }),
+      }}
     >
-      <span className="checkboxSpan">
+      <span style={{ display: "flex", height: "2rem", alignItems: "center" }}>
         {checked != null ? (
           <input
-            className="checkboxInput"
+            style={{ appearance: "checkbox", borderRadius: "50%", marginLeft: "0px" }}
             onChange={() => setAttrs({ checked: !checked })}
             type="checkbox"
             checked={checked}
           />
         ) : isBullet ? (
-          <span className="" />
+          <span style={{ height: "0.45rem", width: "0.45rem", borderRadius: "50%", backgroundColor: "var(--primary-text)"}} />
         ) : (
-          <span className="">{attrs?.label}</span>
+          <span className="orderedDelim">{attrs?.label}</span>
         )}
       </span>
-      <div className="checkboxContent" ref={contentRef} />
+      <div style={{ minWidth: "0" }} ref={contentRef} />
     </li>
   );
 };
