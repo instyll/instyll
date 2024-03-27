39fa251ad7b4fbe51403a5f93193423dee378502
C-FWES <353861933@educ.dpcdsb.org>
2024-03-27 11:26:37 -0400

    add context menu for zap items

--- src/components/zap/zapItem.js
+++ src/components/zap/zapItem.js
@@ -1,14 +1,38 @@
-import { useState } from "react";
-import React from "react";
+/**
+ * @author wou
+ */
+import React, { useState } from "react";
+import { Item, Menu, useContextMenu } from 'react-contexify';
+import 'react-contexify/ReactContexify.css';
 import '../../App.css';
 import ZapOptionsModal from "../../modal/zap/ZapOptionsModal";
 
-import editIcon from '../../icons/editWhite.png'
+import { useDispatch } from "react-redux";
+import editIcon from '../../icons/editWhite.png';
+import UpdateZapModal from "../../modal/zap/UpdateZapModal";
+import { removeZap } from "../../zapSlice";
 
 const ZapItem = ({ zapId }) => {
 
     const [zapOptionsModalOpen, setZapOptionsModalOpen] = useState(false);
     const [selectedZap, setSelectedZap] = useState(null)
+    const [updateZapModalOpen, setUpdateZapModalOpen] = useState(false);
+
+    const dispatch = useDispatch();
+
+    // context menu
+    const { show } = useContextMenu({
+        id: zapId,
+    });
+
+    function handleContextMenu(event){
+        show({
+          event,
+          props: {
+              key: 'value'
+          }
+        })
+    }
 
     const handleZapOptionsModalOpen = (value) => {
         setZapOptionsModalOpen(true);
@@ -16,22 +40,46 @@ const ZapItem = ({ zapId }) => {
 
     const handleClick = ( e ) => {
         e.stopPropagation();
-        console.log("stopped");
-        handleZapOptionsModalOpen(true);
+        show({
+          event,
+          props: {
+              key: 'value'
+          }
+        })
         setSelectedZap(zapId);
-        // console.log(selectedTopic);
       }
+
+      // delete zap
+    const handleRemoveZap = (zapItem) => {
+        dispatch(removeZap(zapItem));
+    }
+
+    const handleClose = () => {
+        setUpdateZapModalOpen(false);
+    }
 
     return(
         <div className="zapItemContainer">
 
-            <ZapOptionsModal
+            {/* <ZapOptionsModal
             show={zapOptionsModalOpen}
             selectedZap={selectedZap}
             onHide={() => setZapOptionsModalOpen(false)}
-            />
+            /> */}
+
+            <UpdateZapModal 
+            show={updateZapModalOpen} 
+            onHide={handleClose}
+            selectedZap={selectedZap}
+            handleClose={handleClose}/>
 
             <span className="zapItemLabel">{zapId}</span>
+
+            <Menu id={zapId}>
+                <Item id="rename" onClick={() => setUpdateZapModalOpen(true)}>Rename</Item>
+                <Item id="delete" onClick={() => handleRemoveZap(selectedZap)}>Delete</Item>
+            </Menu>
+
             <button 
             onClick={handleClick}
             className="zapItemButton"
