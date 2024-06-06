101de7ad4f14a785307fc0a5284632a02803c8f2
C-FWES <353861933@educ.dpcdsb.org>
2024-06-06 14:16:23 -0400

    ui2: use lucide icons in stat container

--- src/components/dock/StatContainer.js
+++ src/components/dock/StatContainer.js
@@ -11,6 +11,8 @@ import { useSelector } from 'react-redux';
 import clockIcon from '../../icons/clock.png';
 import folderIcon from '../../icons/folder.png';
 import wordCountIcon from '../../icons/wordCount.png';
+import { Clock } from 'lucide-react';
+import { BookA } from 'lucide-react';
 
 function StatContainer({ rightPanelOpen, documentPath, wordCount, charCount }) {
 
@@ -26,13 +28,13 @@ function StatContainer({ rightPanelOpen, documentPath, wordCount, charCount }) {
         <div>
             <div className='statContainer'>
                 <p className="paneTitle">Stats</p>
-                <StatItem title={`Words`} icon={wordCountIcon} stat={wordCount}></StatItem>
-                <StatItem title={`Characters`} icon={wordCountIcon} stat={charCount}></StatItem>
+                <StatItem title={`Words`} icon={1} stat={wordCount}></StatItem>
+                <StatItem title={`Characters`} icon={1} stat={charCount}></StatItem>
                 <p className="paneTitle">Properties</p>
-                <StatItem title={`Created`} icon={clockIcon} stat={documentCreated}></StatItem>
-                <StatItem title={`Updated`} icon={clockIcon} stat={documentUpdated}></StatItem>
+                <StatItem title={`Created`} icon={2} stat={documentCreated}></StatItem>
+                <StatItem title={`Updated`} icon={2} stat={documentUpdated}></StatItem>
                 <p className="paneTitle">Location</p>
-                <StatItem title={removePathPrefix(rootPath)} icon={folderIcon}></StatItem>
+                <StatItem title={removePathPrefix(rootPath)} icon={3}></StatItem>
             </div>
         </div>
     );
