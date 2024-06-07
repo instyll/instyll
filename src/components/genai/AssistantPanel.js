b9686fbb81dd9ccd3905518a852090599d4a05a6
C-FWES <353861933@educ.dpcdsb.org>
2024-06-07 14:09:14 -0400

    ui2: resizable input in assistant panel

--- src/components/genai/AssistantPanel.js
+++ src/components/genai/AssistantPanel.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import '../../App.css'
+import TextareaAutosize from 'react-textarea-autosize'
+
+import { ArrowUp } from "lucide-react";
 
 const AssistantPanel = () => {
     return (
         <div className="assistantPanelContainer">
-
+            <div className="assistantPanelInputWrapper">
+                <TextareaAutosize className="dockGenAIInput"></TextareaAutosize>
+                <button className="dockGenAIInputSubmitButton">
+                    <div>
+                        <ArrowUp size={16} style={{marginTop: '2px'}}/>
+                    </div>
+                </button>
+            </div>        
         </div>
     )
 }
