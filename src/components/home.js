c779f9a108bb9b8f5b9585ceba8e687817d11804
C-FWES <353861933@educ.dpcdsb.org>
2024-06-10 13:52:42 -0400

    ui2: center dashboard interaction panel

--- src/components/home.js
+++ src/components/home.js
@@ -280,14 +280,6 @@ const Home = () => {
               <div className='dashboardGenAIInteractionContainer'>
                 <DashboardPanel />
               </div>
-              <div className='recentNotesContainer'>
-                <h1 className='title'>Recent Notes</h1>
-                <div className='recentNoteListWrapper'>
-                  {displayRecentNotes.slice(0, 5).map((recentNote) => (
-                    <DocumentListItem documentInfo={[recentNote[3], recentNote[1]]}></DocumentListItem>
-                  ))}
-                </div>
-              </div>
 
             </div>
 
